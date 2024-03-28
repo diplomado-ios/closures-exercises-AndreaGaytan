db4e22377a5f747c1b10c907385e35e6322d58f7
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-03-28 02:23:23 -0600

    Add first solution

--- closureExercises.playground/Pages/firstChallenge.xcplaygroundpage/Contents.swift
+++ closureExercises.playground/Pages/firstChallenge.xcplaygroundpage/Contents.swift
@@ -5,10 +5,13 @@ import Foundation
 
 /// **Remember to comment line 9 and remove the `//` from line 8 before submitting your code**
 
-//var numbers: [Int] = Array(CommandLine.arguments.dropFirst()).map { Int($0)! }
-var numbers: [Int] = [6, 3, 2, 9, 21, 44]
+var numbers: [Int] = Array(CommandLine.arguments.dropFirst()).map { Int($0)! }
+//var numbers: [Int] = [6, 3, 2, 9, 21, 44]
 
+numbers = numbers.sorted(by: { (s1: Int, s2: Int) -> Bool in
+    return s1 < s2
+})
 
 // Print the sorted array
 
-
+print(numbers)
